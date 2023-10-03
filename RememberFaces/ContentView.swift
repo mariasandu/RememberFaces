8dce41deac9b270fb42b8258f56344cd0ffdbd86
Maria Sandu <Maria.Sandu@emids.com>
2023-10-03 14:08:29 +0300

    Initial Commit

---
+++ RememberFaces/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RememberFaces
+//
+//  Created by Maria Sandu on 03.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RememberFaces/RememberFacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberFacesApp.swift
+//  RememberFaces
+//
+//  Created by Maria Sandu on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RememberFacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
